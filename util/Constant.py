a6928e0ad167ea69dbc28247181611e5c15e94bd
Jiaqi Wang <139347307+myry07@users.noreply.github.com>
2024-03-23 11:55:41 +0100

    Add files via upload

---
+++ util/Constant.py
@@ -0,0 +1,11 @@
+
+class Constant():
+    Screen_Width = 800
+    Screen_Height = 600
+    Screen_Title = "Planegame"
+    absolutePath = "/Users/myry/Documents/MyPythonProject/Games/Planegame/resource/"
+    Plane_Width = 64
+    Plane_Height = 64
+    Shot_Width = 32
+    Shot_Height = 32
+    Score = 0
