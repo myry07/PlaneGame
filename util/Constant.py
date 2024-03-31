d870c969e97fc40ddaf6c75beab69e1c63429c7d
Jiaqi Wang <139347307+myry07@users.noreply.github.com>
2024-03-31 13:11:55 +0200

    Update Constant.py

--- util/Constant.py
+++ util/Constant.py
@@ -3,7 +3,7 @@ class Constant():
     Screen_Width = 800
     Screen_Height = 600
     Screen_Title = "Planegame"
-    absolutePath = "/Users/myry/Documents/MyPythonProject/Games/Planegame/resource/"
+    absolutePath = "-" #your path
     Plane_Width = 64
     Plane_Height = 64
     Shot_Width = 32
